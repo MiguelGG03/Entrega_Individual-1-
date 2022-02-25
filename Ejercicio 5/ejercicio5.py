987506c9d4f4eacce83b9bb385f42b7d1be585cf
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-02-25 16:46:26 +0100

    h

--- Ejercicio 5/ejercicio5.py
+++ Ejercicio 5/ejercicio5.py
@@ -1,5 +1,7 @@
 def vive_en(ciu,cli):
     if(ciu!='TOLEDO'):
         print('No es de Toledo')
+    elif(ciu=='TOLEDO'):
+        print('No es de Toledo')
     if(cli==False):
         print('No es cliente')
